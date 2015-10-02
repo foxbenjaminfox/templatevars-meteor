323a6fabfe8efa8b5bb056cfa2244641a0275521
Benjamin Fox <foxbenjaminfox@gmail.com>
2015-10-02 13:27:14 +0300

    Fix indentation

--- templatevars.js
+++ templatevars.js
@@ -3,22 +3,22 @@ Object.defineProperty(Blaze.TemplateInstance.prototype, 'vars', {
   enumerable: true,
   get: function () {
     const vars = {},
-        proxy = new Proxy(vars, {
-          get (obj, prop) {
-            if (!(prop in obj)) {
-              obj[prop] = new ReactiveVar(null);
+          proxy = new Proxy(vars, {
+            get (obj, prop) {
+              if (!(prop in obj)) {
+                obj[prop] = new ReactiveVar(null);
+                return obj[prop].get();
+              }
               return obj[prop].get();
+            },
+            set (obj, prop, value) {
+              if (prop in obj) {
+                obj[prop].set(value);
+              } else {
+                obj[prop] = new ReactiveVar(value);
+              }
             }
-            return obj[prop].get();
-          },
-          set (obj, prop, value) {
-            if (prop in obj) {
-              obj[prop].set(value);
-            } else {
-              obj[prop] = new ReactiveVar(value);
-            }
-          }
-        });
+          });
     Object.defineProperty(this, 'vars', {
       configureable: true,
       enumerable: true,
